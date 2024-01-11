e422692f14663ddc133046cb0fdfc35bd9c15e7c
PrashantSolcall <prashant.singh@solcall.com.au>
2024-01-11 13:32:58 +0530

    feat: Add AppCenter SDK for analytics and crashes

--- ios/bitnative/AppDelegate.mm
+++ ios/bitnative/AppDelegate.mm
@@ -1,5 +1,8 @@
 #import "AppDelegate.h"
-
+#import <AppCenterReactNativeShared/AppCenterReactNativeShared.h>
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -10,7 +13,11 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
+[AppCenterReactNativeShared setStartAutomatically:YES];
+[AppCenterReactNativeShared setAppSecret:@"951ebece-952f-4f57-b464-d748960e279d"];
+[AppCenterReactNative register];
+[AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+[AppCenterReactNativeCrashes registerWithAutomaticProcessing];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
